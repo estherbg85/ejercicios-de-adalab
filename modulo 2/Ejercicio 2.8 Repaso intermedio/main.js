5c2f7147bfb34c83825062aae1bba973142f9664
estherbg85 <estherbonillagirol85@gmail.com>
2024-12-13 13:02:52 +0100

    agregar tarea

--- modulo 2/Ejercicio 2.8 Repaso intermedio/main.js
+++ modulo 2/Ejercicio 2.8 Repaso intermedio/main.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const btn = document.querySelector('.js_btn');
+const estado = document.querySelector('.js_estado');
+
+const draw = () => {
+    const number = Math.floor(Math.random() * 100);
+    console.log(number);
+    estado.classList.remove('hidden');
+    estado.classList.remove('estado');        
+    if (number % 2 === 0){
+        estado.classList.toggle('estado');
+    }
+    else {
+        estado.classList.toggle('hidden');
+    }
+}
+
+btn.addEventListener('click', (ev) => {
+
+        ev.preventDefault();
+
+        const animo = document.querySelector('.js_animo');       
+        const smile = document.querySelector('.js_smile');
+        smile.innerHTML = animo.value;
+        draw();
+
+})
